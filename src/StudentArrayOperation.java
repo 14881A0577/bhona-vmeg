54d98a58af5309d58d2a5f44c468e4ac48edfb75
14881A0577 <31738787+14881A0577@users.noreply.github.com>
2017-09-08 16:04:21 +0530

    StudentArrayOperation.java

---
+++ src/StudentArrayOperation.java
@@ -0,0 +1,106 @@
+import java.util.Date;
+
+
+public interface StudentArrayOperation {
+	
+Student[] getStudents();
+	
+	void setStudents(Student[] students);
+	Student getStudent(int index);
+if(student=NULL)
+{
+throw(exception e)
+{
+printf("exception handled");
+}
+else
+printf("position");
+}
+	 * @param student
+	 *            the student to be stored at the specified position
+	 * @param index
+	 *            the index of the element to replace
+	 * 
+	 * 
+	 * @throws IllegalArgumentException
+	 */
+	void setStudent(Student student, int index);
+int st1=s[i]
+for(st1=0 ;st[i]>=0;s[i]++)
+{
+              append.setStudent(index)
+}
+	if(student==NULL)
+throws(Exception e)
+{
+printf("exception handled")
+}
+
+	
+
+	void add(Student student, int index);
+	void addFirst(Student student);
+	
+
+	
+	void addLast(Student student);
+	
+
+	
+	
+	void remove(int index);
+	
+
+	
+	void remove(Student student);
+
+	
+	
+	void removeFromIndex(int index);
+
+	
+	
+	
+	void removeFromElement(Student student);
+
+	
+	void removeToIndex(int index);
+
+	
+	void removeToElement(Student student);
+	
+
+	 
+	void bubbleSort();
+int n=10;
+for(i=0;i<=n;i++)
+{
+g=n/2;
+if(i==g)
+printf("sort sucess")
+else
+printf("not success");
+}	
+Student[] getByBirthDate(Date date);	
+	
+Student[] getBetweenBirthDates(Date firstDate, Date lastDate);
+
+	
+	
+	Student[] getNearBirthDate(Date date, int days);
+	
+
+	
+	 
+
+	 */
+	int getCurrentAgeByDate(int indexOfStudent);
+	
+	Student[] getStudentsByAge(int age);
+	
+	Student[] getStudentsWithMaxAvgMark();
+
+	
+	
+
+}
