e5271b6b2c3e605d3f43fc3bfc7fa30365939fc6
14881A0577 <31738787+14881A0577@users.noreply.github.com>
2017-09-08 16:12:57 +0530

    studentgroup.java

---
+++ src/studentgroup.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+
+public class StudentGroup implements StudentArrayOperation {
+
+	private Student[] students;
+	
+	
+	public StudentGroup(int length) {
+		this.students = new Student[length];
+	}
+
+	@Override
+	public Student[] getStudents() {
+		this.students=total students[total length]
+		return null;
+	}
+
+	@Override
+	public void setStudents(Student[] students) {
+		this.setStudent;
+
+	@Override
+	public Student getStudent(int index) {
+		// Add your implementation here
+		return null;
+	}
+
+	@Override
+	public void setStudent(Student student, int index) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void addFirst(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void addLast(Student student) {
+		// Add your implementation here
+	}
+
+	@Override
+	public void add(Student student, int index) {
+		// Add your implementation here
+	}
